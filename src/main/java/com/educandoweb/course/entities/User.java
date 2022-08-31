279629e09f4c4a7d863fd5fce0eaacc0511135bc
Richard Santos <richardfbpa@hotmail.com>
2022-08-31 15:25:41 -0300

    Criando banco h2 e teste

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,9 +3,22 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="usuario")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	//definicao de auto-encremento de id no banco de dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
